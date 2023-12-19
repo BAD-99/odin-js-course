803f4e3fd2614f9b5acab606bfa3f6e0aff9610c
BAD-99 <brandon.andrew.davis@gmail.com>
2023-12-19 05:50:36 -0700

    made main functions

--- object-constructors/script.js
+++ object-constructors/script.js
@@ -1,3 +1,5 @@
+const myLibrary = [];
+
 function Book(title, author, pages, read){
     this.title = title;
     this.author = author;
@@ -9,4 +11,56 @@ function Book(title, author, pages, read){
     }
 }
 
-const hobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '259', false);
+function addBookToLibrary(book){
+    createNewCard(book);
+    myLibrary.push(book);
+}
+
+const deck = document.querySelector('.deck');
+const dialog = document.querySelector('dialog');
+document.querySelector('.new-book').addEventListener('click', ()=>dialog.showModal());
+document.querySelector('dialog > button').addEventListener('click', ()=>dialog.close());
+dialog.querySelector('form > button').addEventListener('click', submitBookFromForm);
+
+function createNewCard(book){
+    let card = document.createElement('div');
+    card.classList.add('card');
+    card.setAttribute('data-index',myLibrary.length);
+    card.innerHTML = `
+    <h1>${book.title}</h1>
+    <div>by ${book.author}</div>
+    <div>Pages: ${book.pages}</div>
+    <div>Read: <input type='checkbox'></div>
+    <button>Remove</button>
+    `;
+    card.querySelector('input').addEventListener('click', ()=>updateReadStatus(card));
+    card.querySelector('button').addEventListener('click', ()=>removeBookFromCard(card));
+    deck.appendChild(card);
+}
+
+function removeBookFromCard(card){
+    let index = card.dataset.index;
+    document.querySelector(`[data-index='${myLibrary.length-1}']`).setAttribute('data-index', index);
+    card.remove();
+    myLibrary[index] = myLibrary[myLibrary.length-1];
+    myLibrary.pop();
+}
+
+function submitBookFromForm(event){
+    event.preventDefault();
+    let book = new Book(
+        document.getElementById('title').value,
+        document.getElementById('author').value,
+        document.getElementById('pages').value,
+        document.getElementById('read').checked
+        );
+    addBookToLibrary(book);
+    dialog.querySelector('form').reset();
+    dialog.close();
+}
+
+function updateReadStatus(card){
+    myLibrary[card.dataset.index].read = card.querySelector('input[type="checkbox"]').checked;
+}
+
+addBookToLibrary(new Book('The Hobbit', 'J.R.R. Tolkien', '259', false));
