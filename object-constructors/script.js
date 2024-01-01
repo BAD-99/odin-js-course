fdf9259627816e8662a344b7a4c421f8e612dffe
BAD-99 <brandon.andrew.davis@gmail.com>
2024-01-01 04:49:38 -0700

    switched book to class

--- object-constructors/script.js
+++ object-constructors/script.js
@@ -1,31 +1,44 @@
 const myLibrary = [];
 
-function Book(title, author, pages, read){
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    this.info = function(){
+// function Book(title, author, pages, read){
+//     this.title = title;
+//     this.author = author;
+//     this.pages = pages;
+//     this.read = read;
+//     this.info = function(){
+//         return `${this.title} by ${this.author}, ${this.pages} pages, `
+//         + (this.read?`has been read.`:`not yet read`);
+//     }
+// }
+
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
+    info = () => {
         return `${this.title} by ${this.author}, ${this.pages} pages, `
-        + (this.read?`has been read.`:`not yet read`);
+                + (this.read?`has been read.`:`not yet read`);
     }
 }
 
-function addBookToLibrary(book){
+function addBookToLibrary(book) {
     createNewCard(book);
     myLibrary.push(book);
 }
 
 const deck = document.querySelector('.deck');
 const dialog = document.querySelector('dialog');
-document.querySelector('.new-book').addEventListener('click', ()=>dialog.showModal());
-document.querySelector('dialog > button').addEventListener('click', ()=>dialog.close());
+document.querySelector('.new-book').addEventListener('click', () => dialog.showModal());
+document.querySelector('dialog > button').addEventListener('click', () => dialog.close());
 dialog.querySelector('form > button').addEventListener('click', submitBookFromForm);
 
-function createNewCard(book){
+function createNewCard(book) {
     let card = document.createElement('div');
     card.classList.add('card');
-    card.setAttribute('data-index',myLibrary.length);
+    card.setAttribute('data-index', myLibrary.length);
     card.innerHTML = `
     <h1>${book.title}</h1>
     <div>by ${book.author}</div>
@@ -33,33 +46,33 @@ function createNewCard(book){
     <div>Read: <input type='checkbox'></div>
     <button>Remove</button>
     `;
-    card.querySelector('input').addEventListener('click', ()=>updateReadStatus(card));
-    card.querySelector('button').addEventListener('click', ()=>removeBookFromCard(card));
+    card.querySelector('input').addEventListener('click', () => updateReadStatus(card));
+    card.querySelector('button').addEventListener('click', () => removeBookFromCard(card));
     deck.appendChild(card);
 }
 
-function removeBookFromCard(card){
+function removeBookFromCard(card) {
     let index = card.dataset.index;
-    document.querySelector(`[data-index='${myLibrary.length-1}']`).setAttribute('data-index', index);
+    document.querySelector(`[data-index='${myLibrary.length - 1}']`).setAttribute('data-index', index);
     card.remove();
-    myLibrary[index] = myLibrary[myLibrary.length-1];
+    myLibrary[index] = myLibrary[myLibrary.length - 1];
     myLibrary.pop();
 }
 
-function submitBookFromForm(event){
+function submitBookFromForm(event) {
     event.preventDefault();
     let book = new Book(
         document.getElementById('title').value,
         document.getElementById('author').value,
         document.getElementById('pages').value,
         document.getElementById('read').checked
-        );
+    );
     addBookToLibrary(book);
     dialog.querySelector('form').reset();
     dialog.close();
 }
 
-function updateReadStatus(card){
+function updateReadStatus(card) {
     myLibrary[card.dataset.index].read = card.querySelector('input[type="checkbox"]').checked;
 }
 
